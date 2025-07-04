ff940db7700f318baee868ec36af4893125b105e
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-04 23:29:01 +0100

    Feat: Editing the text content of the signup page

--- src/app/(external)/auth/sign-in/page.tsx
+++ src/app/(external)/auth/sign-in/page.tsx
@@ -1,8 +1,19 @@
+ 
+import Link from "next/link";
 
 export default function SignIn() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-        <h1 className="text-2xl font-bold">SignIn</h1>
-        </div>
-    )
+        // <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+        //     <h1 className="text-2xl font-bold">SignIn</h1>
+        // </div>
+
+        <main className="flex min-h-screen items-center justify-center">
+            <section className="w-4/5 max-w-5xl">
+                <div>
+                    <h1>SignIn</h1>
+                    <p>Already Have an account? <Link href="#">Login</Link></p>
+                </div>
+            </section>
+        </main>
+    );
 }
