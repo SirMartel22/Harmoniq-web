abe2c2963a4ec4cb49187a65e52c9536e4f6c7d4
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-05 17:50:21 +0100

    Feat: Done the SignUp Section

--- src/app/(external)/auth/sign-in/page.tsx
+++ src/app/(external)/auth/sign-in/page.tsx
@@ -6,17 +6,14 @@ import SigninForm from "@/components/SigninForm"
 
 export default function SignIn() {
     return (
-        // <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-        // <h1 className="text-2xl font-bold">SignIn</h1>
-        // </div>
-
         <div className="flex min-h-screen flex-col items-center justify-center">
             <section className="w-4/5 max-w-5xl">
                 <div className="mb-8 flex flex-col gap-2">
                     <h1 className="text-3xl font-semibold">Welcome to Sign-In Page</h1>
-                    <p className="text-sm text-neutral-500">Already have an account ? <Link href="Login">Login</Link></p>
+                    <p className="text-sm text-neutral-500">Already have an account ? {" "}
+                        <Link href="Login" className="underline underline-offset-4">Login</Link></p>
                 </div>
-
+  
                 <SigninForm />
             </section>
         </div>
--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -1,11 +1,101 @@
 "use client"
 
 import React from 'react'
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import type { FieldPath, Control } from "react-hook-form"
+import { Form, FormField, FormItem, FormControl, FormDescription, FormMessage, FormLabel } from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod"
 
-export default function SigninForm () {
+
+const formSchema = z.object({
+  email: z.string().email(),
+  username: z.string().min(3).max(30),
+  password: z.string().min(8),
+})
+const SigninForm = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password:"",
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
   return (
-      <div> Hello  </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        {/* field */}
+        <SignupFormField
+          name="email"
+          label="Email"
+          placeholder="Email"
+          inputType="Email"
+          formControl={form.control}
+          description=""
+        />
+
+         <SignupFormField
+          name="username"
+          label="Username"
+          placeholder="Username"
+          description="At least 3 characters."
+          formControl={form.control}
+        />
+
+         <SignupFormField
+          name="password"
+          label="Password"
+          placeholder="Password"
+          inputType="password"
+          description="At least 8 characters."
+          formControl={form.control}
+        />
+        <Button type="submit">Sign up</Button>
+      </form>  
+    </Form>
   )
 }
 
-// export default SigninForm
+interface SignupFormFieldProps {
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder: string;
+  description: string;
+  inputType?: string;
+  formControl: Control<z.infer <typeof formSchema>, undefined>
+}
+
+const SignupFormField: React.FC<SignupFormFieldProps> = ({
+  name, label, placeholder, description, inputType, formControl
+}) => {
+
+  return (
+    <FormField 
+      control={formControl}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <input placeholder={placeholder} type={inputType || 'text'}
+              {...field}
+              autoComplete={name}
+            />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    
+      />
+  )
+}
+
+export default SigninForm
